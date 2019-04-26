c8cee148d1dab27afd26f99773d647d600dc98e1
flukkyz <kunakorn.flukky@gmail.com>
2019-04-26 12:48:39 +0700

    Update enter_push.php

--- enter_push.php
+++ enter_push.php
@@ -40,9 +40,9 @@ function pushMsg($arrayHeader,$arrayPostData){
         $arrayPostData['to'] = $id;
         $arrayPostData['messages'][0]['type'] = "text";
         $arrayPostData['messages'][0]['text'] = "ไอ่สัส".$message;
-        $arrayPostData['messages'][1]['type'] = "sticker";
-        $arrayPostData['messages'][1]['packageId'] = "2";
-        $arrayPostData['messages'][1]['stickerId'] = "34";
+        // $arrayPostData['messages'][1]['type'] = "sticker";
+        // $arrayPostData['messages'][1]['packageId'] = "2";
+        // $arrayPostData['messages'][1]['stickerId'] = "34";
         pushMsg($arrayHeader,$arrayPostData);
      }
 
