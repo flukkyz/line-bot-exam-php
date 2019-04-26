af4811848a2c7797350a47c001ccc0d12940a990
flukkyz <kunakorn.flukky@gmail.com>
2019-04-26 12:35:40 +0700

    Create enter_push.php

---
+++ enter_push.php
@@ -0,0 +1,37 @@
+<?php
+
+function pushMsg($arrayHeader,$arrayPostData){
+    $strUrl = "https://api.line.me/v2/bot/message/push";
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL,$strUrl);
+    curl_setopt($ch, CURLOPT_HEADER, false);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrayPostData));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    $result = curl_exec($ch);
+    curl_close ($ch);
+ }
+
+
+   $accessToken = "k6Noqtym0hpj0/fTeTKzviUFm/1loct7qjecS0bixWZ/hEab7oiOSdf/ebaeCVwrW7vlga4yF6ut8U9nt8wIdlOMSV3tsSXjtrteVKzk49O5GEYuRBWhp8n817NY0REiGUNW+tLzRD2IFkEqZFWDAwdB04t89/1O/w1cDnyilFU=";//copy ข้อความ Channel access token ตอนที่ตั้งค่า
+//    $content = file_get_contents('php://input');
+//    $arrayJson = json_decode($content, true);
+   $arrayHeader = array();
+   $arrayHeader[] = "Content-Type: application/json";
+   $arrayHeader[] = "Authorization: Bearer {$accessToken}";
+   $id = "Ucf975b264be9685e1e92c7af1b849b1c";
+   #ตัวอย่าง Message Type "Text + Sticker"
+
+      $arrayPostData['to'] = $id;
+      $arrayPostData['messages'][0]['type'] = "text";
+      $arrayPostData['messages'][0]['text'] = "สวัสดีจ้าาา";
+      $arrayPostData['messages'][1]['type'] = "sticker";
+      $arrayPostData['messages'][1]['packageId'] = "2";
+      $arrayPostData['messages'][1]['stickerId'] = "34";
+      pushMsg($arrayHeader,$arrayPostData);
+
+   
+   exit;
+?>
